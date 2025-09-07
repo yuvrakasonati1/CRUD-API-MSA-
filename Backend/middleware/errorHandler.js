de66369cb0e42923ee4591d018fb2003eeeb31cf
yuvrakasonati1 <yuvrajkesarwani99@gmail.com>
2025-09-07 23:22:04 +0530

    Add files via upload

---
+++ Backend/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+// src/middleware/errorHandler.js
+export default (err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+};
